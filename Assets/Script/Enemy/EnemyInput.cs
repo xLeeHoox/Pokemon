3b20ee696f416ad4855f6f26d9faf4d863966d4a
Viet <>
2023-11-20 21:51:01 +0700

    Dash input for enemy

--- Assets/Script/Enemy/EnemyInput.cs
+++ Assets/Script/Enemy/EnemyInput.cs
@@ -6,6 +6,7 @@ public class EnemyInput : MonoBehaviour
 {
     Enemy enemy;
     MoveByVelocity moveByVelocity;
+    EnemyDash enemyDash;
     public Vector2 direction = Vector2.right;
     public bool isAutoMove = false;
     public bool isSLowed = false;
@@ -13,14 +14,21 @@ public class EnemyInput : MonoBehaviour
     public void OnEnable() //must use in OnEnable because Start method only call one time 
     {
         enemy = GetComponent<Enemy>();
+        enemyDash = GetComponent<EnemyDash>();
         moveByVelocity = enemy.moveByVelocity;
-
     }
+
     public void Update()
     {
+        if (GameManager.Instance.player.isPlayerDead)
+        {
+            GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            return;
+        }
         if (!isAutoMove)
         {
             StopAllCoroutines();
+            StartCoroutine(CallDashByTime(2));
             StartCoroutine(AutoMove());
         }
     }
@@ -37,5 +45,15 @@ public class EnemyInput : MonoBehaviour
     public void RandomMove(Vector2 direction)
     {
         moveByVelocity.CallMoveByVelocity(direction);
+    }
+    public IEnumerator CallDashByTime(float t)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(t);
+            enemyDash.CallDash();
+            yield return new WaitForSeconds(t);
+            enemyDash.StopDash();
+        }
     }
 }
