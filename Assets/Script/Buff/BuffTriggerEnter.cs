0161b186b86d1a6a7acb79a26d71808ac3d4e0c8
Viet <>
2023-11-20 21:46:03 +0700

    Change Buff spawn position and move

--- Assets/Script/Buff/BuffTriggerEnter.cs
+++ Assets/Script/Buff/BuffTriggerEnter.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class BuffTriggerEnter : MonoBehaviour
 {
     [HideInInspector] public BuffSO buffSO;
+    public void Start()
+    {
+        Vector3 startPostiion = this.transform.position;
+        Vector3 targetPosition = new Vector3(startPostiion.x, startPostiion.y - Random.Range(10, 20), startPostiion.z);
+        this.transform.DOMove(targetPosition, 3).SetEase(Ease.InOutSine);
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         IBuffController buffController = collision.GetComponentInParent<IBuffController>();
